8de988ea7b3aa77d808a40f9ee326f9f41f1ae48
dmcdaniel90 <dmcdaniel9@gmail.com>
2024-03-18 10:20:33 +0000

    initial commit

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,121 +1,126 @@
 export type Image = {
-    src: string;
-    alt?: string;
-    caption?: string;
+  src: string;
+  alt?: string;
+  caption?: string;
 };
 
 export type Link = {
-    text: string;
-    href: string;
+  text: string;
+  href: string;
 };
 
 export type Hero = {
-    title?: string;
-    text?: string;
-    image?: Image;
-    actions?: Link[];
+  title?: string;
+  text?: string;
+  image?: Image;
+  actions?: Link[];
 };
 
 export type Subscribe = {
-    title?: string;
-    text?: string;
-    formUrl: string;
+  title?: string;
+  text?: string;
+  formUrl: string;
 };
 
 export type SiteConfig = {
-    logo?: Image;
-    title: string;
-    subtitle?: string;
-    description: string;
-    image?: Image;
-    headerNavLinks?: Link[];
-    footerNavLinks?: Link[];
-    socialLinks?: Link[];
-    hero?: Hero;
-    subscribe?: Subscribe;
-    postsPerPage?: number;
-    projectsPerPage?: number;
+  logo?: Image;
+  title: string;
+  subtitle?: string;
+  description: string;
+  image?: Image;
+  headerNavLinks?: Link[];
+  footerNavLinks?: Link[];
+  socialLinks?: Link[];
+  hero?: Hero;
+  subscribe?: Subscribe;
+  postsPerPage?: number;
+  projectsPerPage?: number;
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+  title: 'SaxCoder',
+  subtitle: 'A blog by Devin McDaniel',
+  description:
+    'A blog about journeying through the world of web development as a professional musician. Written by Devin McDaniel.',
+  image: {
+    src: 'https://placehold.co/1200x628',
+    alt: 'Devin McDaniel - Developer, Musician, and Blogger',
+  },
+  headerNavLinks: [
+    {
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'Projects',
+      href: '/projects',
+    },
+    {
+      text: 'Blog',
+      href: '/blog',
+    },
+    {
+      text: 'Tags',
+      href: '/tags',
+    },
+  ],
+  footerNavLinks: [
+    {
+      text: 'About',
+      href: '/about',
+    },
+    {
+      text: 'Contact',
+      href: '/contact',
+    },
+    {
+      text: 'Terms',
+      href: '/terms',
+    },
+  ],
+  socialLinks: [
+    {
+      text: 'DM Music',
+      href: 'https://devinmcdaniel.com/',
+    },
+    {
+      text: 'Portfolio',
+      href: 'https://devinmcdaniel.tech/',
+    },
+    {
+      text: 'Github',
+      href: 'https://github.com/dmcdaniel90/',
+    },
+    {
+      text: 'Instagram',
+      href: 'https://www.instagram.com/devinmcdanielsax/',
+    },
+    {
+      text: 'X/Twitter',
+      href: 'https://twitter.com/devinmcdaniel90',
+    },
+  ],
+  hero: {
+    title: 'Devin McDaniel - Developer, Musician, and Blogger',
+    text: "I'm **Devin McDaniel**, a web developer and saxophonist at from Texas, USA and now residing in the England, United Kingdom. When I am not waist deep in lines of code (or baby nappies), I entertain crowds playing saxophone for nightclubs and in bands around the country. Feel free to explore some of my coding endeavors on <a href='https://github.com/dmcdaniel90'>GitHub</a> or check out where I am playing next on <a href='https://www.instagram.com/devinmcdanielsax/'>Instagram</a>.",
     image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+      src: 'https://placehold.co/1200x800',
+      alt: 'Devin McDaniel - Developer, Musician, and Blogger',
     },
-    headerNavLinks: [
-        {
-            text: 'Home',
-            href: '/'
-        },
-        {
-            text: 'Projects',
-            href: '/projects'
-        },
-        {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'Tags',
-            href: '/tags'
-        }
+    actions: [
+      {
+        text: 'Get in Touch',
+        href: '/contact',
+      },
     ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
-    ],
-    socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
-    ],
-    hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
-    },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
-    postsPerPage: 8,
-    projectsPerPage: 8
+  },
+  subscribe: {
+    title: 'Subscribe to my newsletter',
+    text: 'Maximum one update per week. All the latest posts directly in your inbox.',
+    formUrl: '#',
+  },
+  postsPerPage: 8,
+  projectsPerPage: 4,
 };
 
 export default siteConfig;
